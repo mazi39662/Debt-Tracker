f1ef4a89882ef5a3da669c0a0b991355a48476d6
TerenceIndenerio <76391538+mazi39662@users.noreply.github.com>
2026-09-18 15:41:31 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'debt-tracker',
+  webDir: 'dist'
+};
+
+export default config;
