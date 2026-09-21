deaacea3cd5f70458ed10604fb111c764ce85e51
TerenceIndenerio <76391538+mazi39662@users.noreply.github.com>
2026-09-22 01:17:37 +0800

    feat: update project structure and configuration for Android, including namespace changes and new MainActivity

--- android/app/src/main/java/com/cypherstudio/debttracker/MainActivity.java
+++ android/app/src/main/java/com/cypherstudio/debttracker/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.cypherstudio.debttracker;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'debt-tracker',
-  webDir: 'dist'
+  appId: "com.cypherstudio.debttracker",
+  appName: "debt-tracker",
+  webDir: "dist",
 };
 
 export default config;
